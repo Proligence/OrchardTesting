7296feb3df98b24977d2c660df35ec01afc364b1
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2013-01-20 23:04:23 +0100

    LoggerMock: Added helper methods for setting formatted message expectations

--- Proligence.Orchard.Testing/Mocks/LoggerMock.cs
+++ Proligence.Orchard.Testing/Mocks/LoggerMock.cs
@@ -24,6 +24,11 @@
             Setup(x => x.Log(LogLevel.Fatal, exception, expectedMessage, null));
         }
 
+        public void ExpectFatalError(string expectedMessage, Exception exception, params object[] args)
+        {
+            Setup(x => x.Log(LogLevel.Fatal, exception, expectedMessage, args));
+        }
+
         public void ExpectError(string expectedMessage)
         {
             Setup(x => x.Log(LogLevel.Error, null, expectedMessage, null));
@@ -34,9 +39,19 @@
             Setup(x => x.Log(LogLevel.Error, exception, expectedMessage, null));
         }
 
+        public void ExpectError(string expectedMessage, Exception exception, params object[] args)
+        {
+            Setup(x => x.Log(LogLevel.Error, exception, expectedMessage, args));
+        }
+
         public void ExpectWarning(string expectedMessage)
         {
             Setup(x => x.Log(LogLevel.Warning, null, expectedMessage, null));
+        }
+
+        public void ExpectWarning(string expectedMessage, params object[] args)
+        {
+            Setup(x => x.Log(LogLevel.Warning, null, expectedMessage, args));
         }
 
         public void ExpectInformation(string expectedMessage)
@@ -44,9 +59,19 @@
             Setup(x => x.Log(LogLevel.Information, null, expectedMessage, null));
         }
 
+        public void ExpectInformation(string expectedMessage, params object[] args)
+        {
+            Setup(x => x.Log(LogLevel.Information, null, expectedMessage, args));
+        }
+
         public void ExpectDebug(string expectedMessage)
         {
             Setup(x => x.Log(LogLevel.Debug, null, expectedMessage, null));
+        }
+
+        public void ExpectDebug(string expectedMessage, params object[] args)
+        {
+            Setup(x => x.Log(LogLevel.Debug, null, expectedMessage, args));
         }
     }
 }

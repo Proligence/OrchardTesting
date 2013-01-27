a42a2d59ae545a3ca4e4f26bbb6687a7d2c63dd2
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2013-01-27 14:36:08 +0100

    Added extension methods for building mock shapes from CombinedResults

---
+++ Proligence.Orchard.Testing/CombinedResultExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace Proligence.Orchard.Testing
+{
+    using System.Linq;
+    using Proligence.Orchard.Testing.Mocks;
+    using global::Orchard.ContentManagement.Drivers;
+
+    public static class CombinedResultExtensions
+    {
+        public static ShapeMock BuildMock(this CombinedResult combinedResult, int index)
+        {
+            DriverResult[] driverResults = combinedResult.GetResults().ToArray();
+            if (driverResults.Length > index)
+            {
+                var shapeResult = driverResults[index] as ContentShapeResult;
+                if (shapeResult != null)
+                {
+                    return shapeResult.BuildShapeMock();
+                }
+            }
+
+            return null;
+        }
+
+        public static ShapeMock BuildMock(this CombinedResult combinedResult, string shapeType)
+        {
+            DriverResult[] driverResults = combinedResult.GetResults().ToArray();
+            foreach (DriverResult driverResult in driverResults)
+            {
+                var shapeResult = driverResult as ContentShapeResult;
+                if ((shapeResult != null) && (shapeResult.GetShapeType() == shapeType))
+                {
+                    return shapeResult.BuildShapeMock();
+                }
+            }
+            
+            return null;
+        }
+    }
+}

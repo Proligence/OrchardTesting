1374aac2a1412960a963e29d29cc5a0bdf6fbe1f
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2013-01-27 15:27:33 +0100

    Added ContentTypePartDefinitionBuilderMock

---
+++ Proligence.Orchard.Testing/Mocks/ContentTypePartDefinitionBuilderMock.cs
@@ -0,0 +1,18 @@
+﻿namespace Proligence.Orchard.Testing.Mocks
+{
+    using global::Orchard.ContentManagement.MetaData.Builders;
+    using global::Orchard.ContentManagement.MetaData.Models;
+
+    public class ContentTypePartDefinitionBuilderMock : ContentTypePartDefinitionBuilder
+    {
+        public ContentTypePartDefinitionBuilderMock(ContentTypePartDefinition part)
+            : base(part)
+        {
+        }
+
+        public SettingsDictionary Settings
+        {
+            get { return _settings; }
+        }
+    }
+}

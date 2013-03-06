117380d8bc13f9f1fc6fe02c62ba0e144cc89f2a
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2013-03-06 21:50:01 +0100

    Added ActivatingFilterExtensions.GetPredicate

---
+++ Proligence.Orchard.Testing/ActivatingFilterExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Proligence.Orchard.Testing
+{
+    using System;
+    using System.Reflection;
+    using global::Orchard.ContentManagement;
+    using global::Orchard.ContentManagement.Handlers;
+
+    public static class ActivatingFilterExtensions
+    {
+        public static Func<string, bool> GetPredicate<T>(this ActivatingFilter<T> filter)
+            where T : ContentPart, new()
+        {
+            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            FieldInfo predicateField = filter.GetType().GetField("_predicate", bindingFlags);
+
+            return (Func<string, bool>)predicateField.GetValue(filter);
+        }
+    }
+}

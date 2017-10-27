cb646fc6426f4819115af4e75416a92b758a0e42
Marek Dzikiewicz <dzikiewiczm@gmail.com>
2017-10-27 08:33:04 +0200

    Suppress XUnit warnings

---
+++ Proligence.Orchard.Testing/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Assertions", "xUnit2005:Do not use identity check on value type", Justification = "<Pending>", Scope = "member", Target = "~M:Proligence.Orchard.Testing.GenericAssert.AreSame``1(``0,``0)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1013:Public method should be marked as test", Justification = "<Pending>", Scope = "member", Target = "~M:Proligence.Orchard.Testing.HttpRoutesTestFixture`1.Setup")]
+
